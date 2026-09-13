019d99f3eac18db81d502b3fa8230054bd22a8e0
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-09-13 18:57:30 -0500

    refactor: keep one reviewed fingerprint projection (jido_con-znq.24)

--- apps/jido_connect/lib/jido_connect/catalog/reviewed_descriptor.ex
+++ apps/jido_connect/lib/jido_connect/catalog/reviewed_descriptor.ex
@@ -1,7 +1,7 @@
 defmodule Jido.Connect.Catalog.ReviewedDescriptor do
   @moduledoc false
 
-  alias Jido.Connect.Catalog.{Fingerprint, Item, Pack, Serializer, ToolDescriptor}
+  alias Jido.Connect.Catalog.{Fingerprint, Item, Pack, Serializer}
 
   @spec project_item(Item.t(), Pack.t()) :: Item.t()
   def project_item(%Item{} = item, %Pack{} = pack) do
@@ -21,39 +21,6 @@ defmodule Jido.Connect.Catalog.ReviewedDescriptor do
       |> Fingerprint.reviewed_descriptor()
 
     %{reviewed | reviewed_fingerprint: fingerprint}
-  end
-
-  @spec project(ToolDescriptor.t(), Pack.t()) :: ToolDescriptor.t()
-  def project(%ToolDescriptor{} = descriptor, %Pack{} = pack) do
-    pack = Pack.reviewed_provenance(pack)
-
-    reviewed = %{
-      descriptor
-      | pack: pack,
-        metadata: Map.merge(descriptor.metadata, reviewed_metadata(descriptor, pack)),
-        reviewed_fingerprint: nil
-    }
-
-    fingerprint =
-      reviewed
-      |> Serializer.to_map()
-      |> Map.delete(:reviewed_fingerprint)
-      |> Fingerprint.reviewed_descriptor()
-
-    %{reviewed | reviewed_fingerprint: fingerprint}
-  end
-
-  defp reviewed_metadata(%ToolDescriptor{} = descriptor, pack) do
-    %{
-      provider: descriptor.tool.provider,
-      package: descriptor.tool.package,
-      pack: pack,
-      risk: descriptor.risk,
-      confirmation: descriptor.confirmation,
-      scopes: descriptor.scopes,
-      policies: Enum.map(descriptor.policies, & &1.id),
-      host_policy_required?: descriptor.host_policy_required?
-    }
   end
 
   defp reviewed_item_metadata(%Item{} = item, pack) do
